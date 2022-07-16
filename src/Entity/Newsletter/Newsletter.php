8e9b530203b38f6a87487e5a0d2eef6c04337729
Elisee-des <esabidani@gmail.com>
2022-07-16 14:21:12 +0000

    Ok pour l'implantation de la base de donnee

---
+++ src/Entity/Newsletter/Newsletter.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity\Newsletter;
+
+use App\Repository\Newsletter\NewsletterRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NewsletterRepository::class)]
+class Newsletter
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'text')]
+    private $content;
+
+    #[ORM\Column(type: 'datetime')]
+    private $created_at;
+
+    #[ORM\Column(type: 'boolean')]
+    private $is_send;
+
+    #[ORM\ManyToOne(targetEntity: Categories::class, inversedBy: 'newsletters')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $categories;
+
+    public function __construct()
+    {
+        $this->created_at = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getIsSend(): ?bool
+    {
+        return $this->is_send;
+    }
+
+    public function setIsSend(bool $is_send): self
+    {
+        $this->is_send = $is_send;
+
+        return $this;
+    }
+
+    public function getCategories(): ?Categories
+    {
+        return $this->categories;
+    }
+
+    public function setCategories(?Categories $categories): self
+    {
+        $this->categories = $categories;
+
+        return $this;
+    }
+}
